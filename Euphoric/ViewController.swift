da147dab6122cf4baded8cd6a3676f8e6330330a
Diego Oruna <diego.orunac@gmail.com>
2020-08-11 10:51:56 -0500

    first commit

---
+++ Euphoric/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Euphoric
+//
+//  Created by Diego Oruna on 11/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
